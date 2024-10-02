4e710523c6178924bdb95a7692b31ebc987d2259
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-10-01 22:47:05 -0300

    fdasd:e

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   output: 'export', // This is used for static export
   reactStrictMode: true,
   distDir: 'dist',
-  images: {
-    unoptimized: true, // This is valid only for static exports
-  },
 }
 
 module.exports = nextConfig
